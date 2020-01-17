38628c85145fcc2a7fc63f17567b4812f7691201
zalogu <59750242+zalogu@users.noreply.github.com>
2020-01-17 16:10:35 -0600

    Add files via upload

--- src/main/java/frc/input/InputMethod.java
+++ src/main/java/frc/input/InputMethod.java
@@ -1,14 +1,18 @@
-package frc.input;
+package src.main.java.frc.input;
 
 /**
  * Operator controls for the robot
  */
 public abstract class InputMethod {
-  public double leftSidePower() {
-    return 0;
-  }
+    public double leftSidePower() {
+        return 0;
+    }
 
-  public double rightSidePower() {
-    return 0;
-  }
+    public double rightSidePower() {
+        return 0;
+    }
+    
+    public boolean intake() {
+        return false;
+    }
 }
