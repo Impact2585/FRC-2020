324bf827e82b5f5efa386519a601489a42a367c4
ProCoder9000 <39925215+ProCoder9000@users.noreply.github.com>
2020-07-10 16:38:19 -0500

    Github Practice

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,7 +4,7 @@ package frc.robot;
  * Maps input/output pins on the RoboRIO
  */
 public class RobotMap {
-  public static final int LEFT_DRIVE_MOTOR = 0;
+  public static final int LEFT_DRIVE_MOTOR = 1;
   public static final int RIGHT_DRIVE_MOTOR = 1;
 
 }
